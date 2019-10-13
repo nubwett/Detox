8f124e7427ec41845801afb28e08fc8a4fb87b83
Allen Su <allensu2001@gmail.com>
2019-10-12 23:21:33 -0700

    made thing change text

--- content.js
+++ content.js
@@ -1,3 +1,28 @@
-const wrap = document.body;
+/*const wrap = document.body;
 console.log(wrap);
 
+for each dom in the doc body
+    send dom info to api
+    api returns something
+    changes inner html to censor
+*/
+
+
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/cheese/gi, 'chick');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
